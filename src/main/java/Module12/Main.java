73ef9a664b8d347553dd68b900d45f12177da1b4
Aleksandr Rogozhkin <rogozkin371@gmail.com>
2023-03-08 19:43:41 +0200

    innit

--- src/main/java/Module12/Main.java
+++ src/main/java/Module12/Main.java
@@ -1,11 +1,20 @@
 package Module12;
 
 public class Main {
-    public static void main(String[] args) {
-       Thread thread=new Thread(CounterSeconds::everyOneSecond);
-       Thread thread1=new Thread(CounterSeconds::everyFiveSecond);
-       thread.start();
-       thread1.start();
+    public static void main(String[] args){
+        System.out.println("Time counter");
+        Thread thread2=new Thread(CounterSeconds::everyOneSecond);
+        Thread thread1=new Thread(CounterSeconds::everyFiveSecond);
+        thread2.start();
+        thread1.start();
+
+
+
+
+
+
+
+
 
 
     }
