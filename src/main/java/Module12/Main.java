d1bb4c68652d2f0292cac71504b3654bbefad571
Aleksandr Rogozhkin <rogozkin371@gmail.com>
2023-03-08 18:33:21 +0200

    innit

--- src/main/java/Module12/Main.java
+++ src/main/java/Module12/Main.java
@@ -2,14 +2,11 @@ package Module12;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Thread.currentThread().getName());
+       Thread thread=new Thread(CounterSeconds::everyOneSecond);
+       Thread thread1=new Thread(CounterSeconds::everyFiveSecond);
+       thread.start();
+       thread1.start();
 
-
-        Thread thread1=new Thread(()->{
-            System.out.println(Thread.currentThread().getName());
-            System.out.println(Thread.currentThread().getId());
-        });
-        thread1.start();
 
     }
 }
